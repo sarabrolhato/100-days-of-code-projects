b6c5afff7b7109975b05fd983f226c2c88aa848f
Sara Brolhato <sarabrolhato@gmail.com>
2024-12-23 11:49:27 -0300

    Adds day 8.

---
+++ day08/README.md
@@ -0,0 +1,7 @@
+### Day 8 - Function Parameters and Caesar Cipher
+
+- Day 8 Project: Caesar Cipher
+
+### Day 8 Resources
+
+- [Python List index() Method](https://www.w3schools.com/python/ref_list_index.asp)
---
+++ day08/main.py
@@ -0,0 +1,34 @@
+# Caesar Cipher
+
+# This project creates and deciphers encrypted messages using the Caesar Cipher.
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k' , 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+    for letter in original_text:
+        if letter not in alphabet:
+            output_text += letter
+        else:
+            output_text += alphabet[(alphabet.index(letter) + shift_amount) % len(alphabet)]
+                    
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+    
+
+should_continue = True
+
+while should_continue:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type your shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise, type 'no'.\n").lower()
+    if restart == "no":
+        should_continue = False
+        print("Goodbye!")
